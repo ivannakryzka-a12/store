3256b110a2465c3f8fb146f39b163366ef271411
Кризька Іванна <ivanna.kryzka@nure.ua>
2026-04-27 19:20:09 +0300

    feat: implement check model

--- store/Models/Check.cs
+++ store/Models/Check.cs
@@ -4,7 +4,25 @@ using System.Text;
 
 namespace Store.Models
 {
-    internal class Check
+    /// <summary>
+    /// Чек покупки.
+    /// </summary>
+    public class Check
     {
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public List<CheckItem> Items { get; set; } = new();
+
+        /// <summary> Обчислює загальну суму чеку. </summary>
+        public decimal TotalAmount()
+        {
+            decimal result = 0;
+
+            foreach (var item in Items)
+            {
+                result += item.Sum;
+            }
+
+            return result;
+        }
     }
 }
