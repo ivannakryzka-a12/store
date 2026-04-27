cd27ca7eaa4dffc9bd6ddf1861cc4da7561f6cbe
Кризька Іванна <ivanna.kryzka@nure.ua>
2026-04-27 18:23:43 +0300

    Updated Product model

--- store/Models/Product.cs
+++ store/Models/Product.cs
@@ -4,7 +4,25 @@ using System.Text;
 
 namespace Store.Models
 {
-    internal class Product
+    /// <summary>
+    /// Опис одного товару магазину.
+    /// </summary>
+    public class Product
     {
+        public string Name { get; set; } = "";
+        public string Unit { get; set; } = "";
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public DateTime LastDeliveryDate { get; set; } = DateTime.Now;
+
+        public decimal GetStockValue()
+        {
+            return Quantity * Price;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} ({Quantity} {Unit})";
+        }
     }
 }
