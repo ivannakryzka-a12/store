edac26dadaad50bd5f724ab7f4916ffe35c5d1c4
Кризька Іванна <ivanna.kryzka@nure.ua>
2026-04-27 18:48:44 +0300

    feat: add check item model

---
+++ store/Models/CheckItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Store.Models
+{
+    /// <summary>
+    /// Один рядок у чеку.
+    /// </summary>
+    public class CheckItem
+    {
+        public string ProductName { get; set; } = "";
+        public int Count { get; set; }
+        public decimal Price { get; set; }
+
+        public decimal Sum => Count * Price;
+    }
+}
