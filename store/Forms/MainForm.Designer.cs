22efdc0a705cc28555d4c7d25f1b2c2f9c20f1af
Кризька Іванна <ivanna.kryzka@nure.ua>
2026-05-11 17:57:51 +0300

    add: main form design

--- store/Forms/MainForm.Designer.cs
+++ store/Forms/MainForm.Designer.cs
@@ -28,12 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label1.ForeColor = Color.FromArgb(70, 40, 40);
+            label1.Location = new Point(230, 50);
+            label1.Name = "label1";
+            label1.Size = new Size(221, 41);
+            label1.TabIndex = 0;
+            label1.Text = "Облік товарів";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(120, 0, 0);
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(230, 130);
+            button1.Name = "button1";
+            button1.Size = new Size(220, 45);
+            button1.TabIndex = 1;
+            button1.Text = "Товари";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(120, 0, 0);
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
+            button2.ForeColor = Color.White;
+            button2.Location = new Point(230, 190);
+            button2.Name = "button2";
+            button2.Size = new Size(220, 45);
+            button2.TabIndex = 2;
+            button2.Text = "Надходження";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(120, 0, 0);
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
+            button3.ForeColor = Color.White;
+            button3.Location = new Point(230, 250);
+            button3.Name = "button3";
+            button3.Size = new Size(220, 45);
+            button3.TabIndex = 3;
+            button3.Text = "Продаж";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.FromArgb(120, 0, 0);
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
+            button4.ForeColor = Color.White;
+            button4.Location = new Point(230, 310);
+            button4.Name = "button4";
+            button4.Size = new Size(220, 45);
+            button4.TabIndex = 4;
+            button4.Text = "Інвентаризація";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Text = "MainForm";
+            BackColor = Color.FromArgb(248, 240, 235);
+            ClientSize = new Size(682, 453);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "MainForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Головне меню";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
     }
 }
